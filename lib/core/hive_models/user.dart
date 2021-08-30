3ad8ea7bbf04e12e7d8de21e8d7b3aa5f50a8b8b
smgks <vetosmg@gmail.com>
2021-08-30 21:11:34 +1000

    Move hive user model to core

--- lib/core/hive_models/user.dart
+++ lib/core/hive_models/user.dart
@@ -1,5 +1,6 @@
 import 'package:equatable/equatable.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:random_string/random_string.dart';
 
 part 'user.g.dart';
 
@@ -7,11 +8,16 @@ part 'user.g.dart';
 class User extends Equatable {
   User({
     required this.username,
-  });
+  }) : idString = randomNumeric(16);
 
   @HiveField(0)
   final String username;
 
+  @HiveField(1)
+  final String idString;
+
   @override
-  List<Object?> get props => [username];
+  List<Object?> get props => [username, idString];
+
+  factory User.fromString(String username) => User(username: username);
 }
